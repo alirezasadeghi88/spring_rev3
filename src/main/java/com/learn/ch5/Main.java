8975f7b2e50d79a4012b496293b44d32cca310e8
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-08 02:05:18 -0700

    add main

--- src/main/java/com/learn/ch5/Main.java
+++ src/main/java/com/learn/ch5/Main.java
@@ -10,13 +10,6 @@ public class Main {
         var c =
                 new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        var s1 = c.getBean( CommentService.class);
-        var s2 = c.getBean(UserService.class);
 
-        boolean b1 =
-                s1.getCommentRepository() == s2.getCommentRepository();
-
-
-        System.out.println(b1);
     }
 }
--- src/main/java/com/learn/ch5/config/ProjectConfig.java
+++ src/main/java/com/learn/ch5/config/ProjectConfig.java
@@ -5,8 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.learn.ch5.service",
-        resourcePattern = "com.learn.ch5.repository")
+@ComponentScan(basePackages = "com.learn.ch5.service")
 public class ProjectConfig {
 
 
