fc3c01b994012194cd805fac818409ef82230efe
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-09 02:20:25 -0700

    add repository

---
+++ src/main/java/com/learn/ch5/prototype/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.learn.ch5.prototype.repository;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class CommentRepository {
+}
