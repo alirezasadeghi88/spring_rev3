7340ee02f71d7ed3d7b36f7f6ff3ee6be3a4d4c7
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-09 02:26:53 -0700

    add service

---
+++ src/main/java/com/learn/ch5/prototype/service/UserService.java
@@ -0,0 +1,16 @@
+package com.learn.ch5.prototype.service;
+
+import com.learn.ch5.singleton.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public CommentRepository getCommentRepository() {
+        return commentRepository;
+    }
+}
