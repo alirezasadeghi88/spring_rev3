c22825379a4b5b73731db9cdd639c2796fce0a32
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-09 02:24:48 -0700

    add config

--- src/main/java/com/learn/ch5/prototype/config/ProjectConfig.java
+++ src/main/java/com/learn/ch5/prototype/config/ProjectConfig.java
@@ -3,15 +3,14 @@ package com.learn.ch5.prototype.config;
 import com.learn.ch5.prototype.service.CommentService;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan(basePackages = {"com.learn.ch5.prototype.service",
+"com.learn.ch5.prototype.repository"})
 public class ProjectConfig {
 
-    @Bean
-    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
-    public CommentService commentService() {
-        return new CommentService();
-    }
+
 }
