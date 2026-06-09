a08c0d57bcc1748084fe703971d8a58d967894c7
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-09 02:01:07 -0700

    add main

--- src/main/java/com/learn/ch5/prototype/Main.java
+++ src/main/java/com/learn/ch5/prototype/Main.java
@@ -1,4 +1,19 @@
 package com.learn.ch5.prototype;
 
+import com.learn.ch3.config.ProjectConfig;
+import com.learn.ch5.prototype.service.CommentService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        var c =
+                new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var cs1 = c.getBean("commentService", CommentService.class);
+        var cs2 = c.getBean("commentService", CommentService.class);
+
+        boolean b1 = cs1 == cs2;
+
+        System.out.println(b1);
+    }
 }
